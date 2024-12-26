980fe4ec82d0d099725b94e821a6a412fd5f375f
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-12-26 17:12:06 -0300

    Criando Cloud Gateway

---
+++ mscloudgateway/src/test/java/victorMarchiDev/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package victorMarchiDev.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
