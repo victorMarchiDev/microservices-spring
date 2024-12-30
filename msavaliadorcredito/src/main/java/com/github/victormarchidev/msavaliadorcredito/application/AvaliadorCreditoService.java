56bb721e1344d423548212a25f9522a7d86dafd1
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-12-30 16:52:20 -0300

    Buscando os dados de cartoes pelo microservice de cartoes via comunicacao Feign

--- msavaliadorcredito/src/main/java/com/github/victormarchidev/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ msavaliadorcredito/src/main/java/com/github/victormarchidev/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,24 +1,31 @@
 package com.github.victormarchidev.msavaliadorcredito.application;
 
+import com.github.victormarchidev.msavaliadorcredito.domain.model.CartaoCliente;
 import com.github.victormarchidev.msavaliadorcredito.domain.model.DadosCliente;
 import com.github.victormarchidev.msavaliadorcredito.domain.model.SituacaoCliente;
+import com.github.victormarchidev.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import com.github.victormarchidev.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import feign.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AvaliadorCreditoService {
 
     private final ClienteResourceClient clientesClient;
+    private final CartoesResourceClient cartoesClient;
 
     public SituacaoCliente obterSituacaoCliente(String cpf) {
         ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
+        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesCliente(cpf);
         return SituacaoCliente
                 .builder()
                 .cliente(dadosClienteResponse.getBody())
+                .cartoes(cartoesResponse.getBody())
                 .build();
     }
 
