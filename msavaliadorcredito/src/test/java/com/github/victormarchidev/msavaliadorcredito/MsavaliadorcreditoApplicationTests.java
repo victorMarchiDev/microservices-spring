8f325f3443a4ff0eb59dd65853b825185f10aab7
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2024-12-30 14:39:29 -0300

    ajustando estrutura do projeto

---
+++ msavaliadorcredito/src/test/java/com/github/victormarchidev/msavaliadorcredito/MsavaliadorcreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.victormarchidev.msavaliadorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcreditoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
