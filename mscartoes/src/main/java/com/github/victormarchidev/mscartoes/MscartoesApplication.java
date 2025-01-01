f0f8cb4662231b979df233ffb3bb6292850c1f96
Victor Marchi <101611816+victorMarchiDev@users.noreply.github.com>
2025-01-01 13:30:42 -0300

    Instalando RabbitMQ via docker e configurando o mscartoes para fila de mensagens

--- mscartoes/src/main/java/com/github/victormarchidev/mscartoes/MscartoesApplication.java
+++ mscartoes/src/main/java/com/github/victormarchidev/mscartoes/MscartoesApplication.java
@@ -1,9 +1,11 @@
 package com.github.victormarchidev.mscartoes;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableRabbit
 public class MscartoesApplication {
 
     public static void main(String[] args) {
